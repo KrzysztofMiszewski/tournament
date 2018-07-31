937ae84d838842b835af7961b115976f507e93b6
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-07-31 20:51:25 +0200

    frontend

--- src/main/java/tournament/model/Tournament.java
+++ src/main/java/tournament/model/Tournament.java
@@ -1,4 +1,13 @@
 package tournament.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Tournament {
+    @Id
+    @GeneratedValue
+    private Long id;
+
 }
