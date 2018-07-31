e532a09d004c85adfad7a8ff7fd29a5ab608311b
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-07-31 20:05:53 +0200

    participant

--- src/main/java/tournament/model/Participant.java
+++ src/main/java/tournament/model/Participant.java
@@ -1,4 +1,37 @@
 package tournament.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class Participant {
-}
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @Column(unique = true)
+        private String mail;
+
+        private String password;
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getMail() {
+            return mail;
+        }
+
+        public void setMail(String mail) {
+            this.mail = mail;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
