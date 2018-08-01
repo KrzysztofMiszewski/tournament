ed765661def323394ddd1bb914c49eccc34d4664
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-08-01 20:49:53 +0200

    User

--- src/main/java/tournament/service/impl/UserServiceImpl.java
+++ src/main/java/tournament/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package tournament.service.impl;
 
 
+import org.springframework.stereotype.Service;
 import tournament.model.User;
 import tournament.repository.UserRepository;
 import tournament.service.UserService;
@@ -8,6 +9,7 @@ import tournament.service.UserService;
 
 import java.util.Set;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
