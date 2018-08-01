fc53d067d0905874df7c5d0afe5d0264a2adad6a
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-08-01 20:45:24 +0200

    gameServiceImpl

--- src/main/java/tournament/repository/GameRepository.java
+++ src/main/java/tournament/repository/GameRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tournament.model.Game;
 import tournament.model.Participant;
+import tournament.model.Tournament;
 
 @Repository
 public interface GameRepository extends CrudRepository <Game, Long> {
 
     Game showCurrentGame(Game game);
     void selectWinner(Game game, Participant winner);
+    void create(Tournament tournament, int round, int gameNumber);
 }
