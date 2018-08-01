aafff85b8d76c7340da920db173e1d6e689aa570
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-08-01 19:52:18 +0200

    gameService i Repo

--- src/main/java/tournament/model/Game.java
+++ src/main/java/tournament/model/Game.java
@@ -3,6 +3,7 @@ package tournament.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Game {
@@ -10,4 +11,65 @@ public class Game {
     @GeneratedValue
     private Long id;
 
+    private Participant white;
+    private Participant black;
+    private String winner;
+    private int round;
+    private int gameNumber;
+
+    @ManyToOne
+    private Tournament tournament;
+
+    public Tournament getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+    public Long getId() {
+        return id;
+
+    }
+
+    public Participant getWhite() {
+        return white;
+    }
+
+    public void setWhite(Participant white) {
+        this.white = white;
+    }
+
+    public Participant getBlack() {
+        return black;
+    }
+
+    public void setBlack(Participant black) {
+        this.black = black;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+    public int getGameNumber() {
+        return gameNumber;
+    }
+
+    public void setGameNumber(int gameNumber) {
+        this.gameNumber = gameNumber;
+    }
 }
--- src/main/java/tournament/service/impl/GameServiceImpl.java
+++ src/main/java/tournament/service/impl/GameServiceImpl.java
@@ -1,4 +1,44 @@
 package tournament.service.impl;
 
-public class GameServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tournament.model.Game;
+import tournament.model.Participant;
+import tournament.model.Tournament;
+import tournament.repository.GameRepository;
+import tournament.service.GameService;
+
+@Service
+public class GameServiceImpl implements GameService {
+
+    private GameRepository gameRepository;
+
+    @Autowired
+    public GameServiceImpl(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    @Override
+    public Game showCurrentGame(Game game) {
+        return null;
+    }
+
+    @Override
+    public void selectWinner(Game game, Participant winner) {
+        if (isFinal(game) == true ) {
+           Tournament tournament = game.getTournament();
+
+        }
+
+        gameRepository.save();
+    }
+
+    public boolean isFinal (Game game){
+        if (game.getRound() == 0){
+            return true;
+        }
+        else
+            return false;
+    }
+
 }
