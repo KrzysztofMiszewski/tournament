d792286e6e51ce47728bc6660b075176291ce297
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-08-01 20:04:32 +0200

    participant

--- src/main/java/tournament/model/Participant.java
+++ src/main/java/tournament/model/Participant.java
@@ -10,12 +10,40 @@ import java.util.Set;
 public class Participant {
 
     private Long Id;
+    
     private String mail;
     private String nick;
 
 
     @ManyToMany(mappedBy ="participants")
     private Set< Tournament > tournaments;
+    public Long getId() {
+        return Id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public Set<Tournament> getTournaments() {
+        return tournaments;
+    }
+
+    public void setTournaments(Set<Tournament> tournaments) {
+        this.tournaments = tournaments;
+    }
 
 
 }
