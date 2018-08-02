74bdf8d2cb2c8703d676056558796808240afd36
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-08-02 19:19:25 +0200

    ParticipantDto

--- src/main/java/tournament/repository/ParticipantRepository.java
+++ src/main/java/tournament/repository/ParticipantRepository.java
@@ -1,10 +1,12 @@
 package tournament.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tournament.model.Participant;
 
 import java.util.Set;
 
+@Repository
 public interface  ParticipantRepository extends CrudRepository<Participant,Long> {
 Set<Participant> findAllByMail(String mail);
 Set<Participant> findAllBy();
--- src/main/java/tournament/service/ParticipantService.java
+++ src/main/java/tournament/service/ParticipantService.java
@@ -1,8 +1,10 @@
 package tournament.service;
 
+import org.springframework.stereotype.Service;
 import tournament.model.Participant;
 import tournament.model.Tournament;
 
+@Service
 public interface ParticipantService {
  void Join(Tournament tournament, String nick, String mail);
   Participant participant = new Participant();
