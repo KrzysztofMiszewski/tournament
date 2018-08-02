82f0dd71d611d727afd2b6c9d7a3d85b55c8bf81
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-08-02 18:00:01 +0200

    Merge branch 'master' of https://github.com/KrzysztofMiszewski/tournament
    # Conflicts:
    #	src/main/java/tournament/service/impl/GameServiceImpl.java

--- src/main/java/tournament/repository/ParticipantRepository.java
+++ src/main/java/tournament/repository/ParticipantRepository.java
@@ -1,4 +1,14 @@
 package tournament.repository;
 
-public class ParticipantRepository {
+import org.springframework.data.repository.CrudRepository;
+import tournament.model.Participant;
+
+import java.util.Set;
+
+public interface  ParticipantRepository extends CrudRepository<Participant,Long> {
+Set<Participant> findAllByMail(String mail);
+Set<Participant> findAllBy();
+Set<Participant> findAllByNick(String nick);
+
+
 }
