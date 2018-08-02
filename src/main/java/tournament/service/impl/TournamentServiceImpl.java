9d9788079f86e554673eb112d3e6eedb93189edb
Krzysztof Miszewski <krzysztofxmiszewski@gmail.com>
2018-08-02 14:36:40 +0200

    createGames i randomiseParticipants

--- src/main/java/tournament/service/impl/TournamentServiceImpl.java
+++ src/main/java/tournament/service/impl/TournamentServiceImpl.java
@@ -2,22 +2,29 @@ package tournament.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import tournament.model.Game;
+import tournament.model.Participant;
 import tournament.model.Tournament;
 import tournament.model.User;
+import tournament.repository.GameRepository;
 import tournament.repository.TournamentRepository;
 import tournament.service.GameService;
 import tournament.service.TournamentService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class TournamentServiceImpl implements TournamentService {
 
     private TournamentRepository tournamentRepository;
+    private GameRepository gameRepository;
     private GameService gameService;
 
     @Autowired
-    public TournamentServiceImpl(TournamentRepository tournamentRepository, GameService gameService) {
+    public TournamentServiceImpl(TournamentRepository tournamentRepository, GameRepository gameRepository, GameService gameService) {
         this.tournamentRepository = tournamentRepository;
+        this.gameRepository = gameRepository;
         this.gameService = gameService;
     }
 
@@ -49,17 +56,48 @@ public class TournamentServiceImpl implements TournamentService {
     @Override
     @Transactional
     public void start(Tournament tournament) {
+        int rounds = createGames(tournament);
+        randomiseParticipants(tournament, rounds);
+        autoResolveWildcards(tournament, rounds);
 
     }
 
-    private void createGames(Tournament tournament) {
+    private void autoResolveWildcards(Tournament tournament, int rounds) {
+        //TODO
+    }
+
+    private void randomiseParticipants(Tournament tournament, int rounds) {
+        List<Participant> participants = new ArrayList<>(tournament.getParticipants());
+        int games = (int) Math.pow(2, rounds);
+        Game game;
+        for (int i = 0; i < games; i += 2) {
+            int index = (int) (Math.random() * participants.size());
+            game = gameRepository.findOneByRoundAndGameNumber(rounds - 1, i);
+            game.setWhite(participants.remove(index));
+            gameRepository.save(game);
+        }
+        for (int i = 1; i < games && !participants.isEmpty(); i += 2) {
+            int index = (int) (Math.random() * participants.size());
+            game = gameRepository.findOneByRoundAndGameNumber(rounds - 1, i);
+            game.setBlack(participants.remove(index));
+            gameRepository.save(game);
+        }
+    }
+
+    private int createGames(Tournament tournament) {
         int participants = tournament.getParticipants().size();
-        int rounds = 0;
+        int rounds = 1;
         int games = 1;
         while (participants > games * 2) {
             games *= 2;
             rounds++;
         }
-
+        for (int row = 0; row < rounds; row++) {
+            for (int game = 0; game < (row+1)*2; game++) {
+                gameService.create(tournament, row, game);
+            }
+        }
+        tournamentRepository.save(tournament);
+        return rounds;
     }
 }
