3e287d59292285818951f45ba380319eb24c0460
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-08-03 17:16:27 +0200

    dto

--- src/main/java/tournament/dto/UserDto.java
+++ src/main/java/tournament/dto/UserDto.java
@@ -4,33 +4,20 @@ import tournament.model.User;
 
 public class UserDto {
 
-    private Long id;
-    private String login;
-    private String password;
 
-    public UserDto(Long id, String login, String password) {
-        this.id = id;
+    private String login;
+
+
+    public UserDto(String login) {
         this.login = login;
-        this.password = password;
+
     }
 
     public UserDto(User user){
-    this.id = user.getId();
     this.login = user.getLogin();
-    this.password = user.getPassword();
-    }
-
-    public Long getId(){
-        return id;
     }
 
     public String getLogin(){
         return login;
     }
-
-    public String getPassword(){
-        return password;
     }
-
-
-}
