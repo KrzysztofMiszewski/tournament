cd36dd65dfe0378d9b41f94ac2d2c9bc344f5bab
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-08-07 20:26:58 +0200

    com

--- src/main/java/tournament/repository/UserRepository.java
+++ src/main/java/tournament/repository/UserRepository.java
@@ -12,10 +12,5 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Set<User> findAll();
     Set<User> findByLogin(String login);
     Set<User> findOneById(Long id);
-<<<<<<< HEAD
-=======
     User findOneByLogin(String login);
-
-
->>>>>>> 1835e53a4414e9f7f3308b0c509ef070ef07df73
 }
